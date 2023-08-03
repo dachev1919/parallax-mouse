a1da9b82be2badf54639e85f6cad6c3fc162f04a
Dachev <dachev1919@gmail.com>
2023-08-03 12:58:43 +0300

    minor changes

--- src/modules/home/pages/Home.jsx
+++ src/modules/home/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import MagicList from "../components/magic-list/MagicList";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <MagicList />
+    </>
   );
 };
 
