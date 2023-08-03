1fd16d29a6399de332d51d784678dae5e5d09e25
Dachev <dachev1919@gmail.com>
2023-08-03 12:58:32 +0300

    added mock data

---
+++ src/common/mock/data.js
@@ -0,0 +1,33 @@
+import img1 from '../assets/images/1.jpg';
+import img2 from '../assets/images/2.jpg';
+import img3 from '../assets/images/3.jpg';
+import img4 from '../assets/images/4.jpg';
+import img5 from '../assets/images/5.jpg';
+
+export const ITEMS = [
+  {
+    id: 1,
+    image: img1,
+    title: 'First Header'
+  },
+  {
+    id: 2,
+    image: img2,
+    title: 'Second Header'
+  },
+  {
+    id: 3,
+    image: img3,
+    title: 'Third Header'
+  },
+  {
+    id: 4,
+    image: img4,
+    title: 'Fourth Header'
+  },
+  {
+    id: 5,
+    image: img5,
+    title: 'Fifth Header'
+  }
+];
