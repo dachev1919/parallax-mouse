7e8eee434e2f32bf2878e2d716c406e07493e455
Dachev <dachev1919@gmail.com>
2023-08-03 13:58:55 +0300

    validate js

--- src/modules/home/components/magic-list/MagicList.jsx
+++ src/modules/home/components/magic-list/MagicList.jsx
@@ -1,8 +1,19 @@
 import styles from './MagicList.module.scss';
 import { ITEMS } from "../../../../common/mock/data";
 import MagicListItem from "./MagicListItem";
+import { useEffect } from "react";
 
 const MyComponent = () => {
+  const eventHandler = (e) => {
+    document.body.style.cssText = `--move-x: ${e.clientX}px;--move-y: ${e.clientY}px`;
+  }
+  useEffect(() => {
+    document.addEventListener('mousemove', eventHandler);
+
+    return () => {
+      document.removeEventListener('mousemove', eventHandler);
+    };
+  }, []);
 
   return (
     <section className={styles['magic-list']}>
--- src/modules/home/components/magic-list/MagicListItem.jsx
+++ src/modules/home/components/magic-list/MagicListItem.jsx
@@ -1,30 +1,15 @@
 import styles from './MagicList.module.scss';
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../../../utils/constants";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const MagicListItem = ({image, title}) => {
-  const cursorRef = useRef();
-  const eventHandler = (e) => {
-    if (cursorRef && cursorRef.current) {
-      cursorRef.current.style.cssText = `--move-x: ${e.clientX}px;--move-y: ${e.clientY}px`;
-    }
-  }
-  useEffect(() => {
-    document.addEventListener('mousemove', eventHandler);
-
-    return () => {
-      document.removeEventListener('mousemove', eventHandler);
-    };
-  }, []);
-
-
   return (
     <Link to={ROUTES.HOME} className={styles.item}>
       <div className={styles.header}>
         {title}
       </div>
-      <div ref={cursorRef} className={styles.cursor}>
+      <div className={styles.cursor}>
         <div className={styles.image} style={{backgroundImage: `url(${image})`}}></div>
       </div>
     </Link>
